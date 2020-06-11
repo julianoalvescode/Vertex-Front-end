e6110f0bb273306daf6ea4e4233db3f5f6643ef6
Juliano Alves <contato.julianoalvess@gmail.com>
2020-06-11 16:08:09 -0300

    feat: add new key api

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // API Youtube Data v3
-export const KEY = 'AIzaSyDiLOH2TZkcehQGkqfDp6u7u0nE5J9NXYw';
+export const KEY = 'AIzaSyB-IuCiQg73-JkklFcYlmistb0K-2FpigI';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/', // Link the of Endpoint
